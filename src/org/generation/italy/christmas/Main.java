9e68b51d24d9630c00d3247172a609149484c084
ErJump <fedelijunior@gmail.com>
2022-11-23 11:31:03 +0100

    feat: :sparkles: Ex. completed
    Adds wishList list and while loop

---
+++ src/org/generation/italy/christmas/Main.java
@@ -0,0 +1,32 @@
+package org.generation.italy.christmas;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		List<String> wishList = new ArrayList<>();
+		boolean goNext = true;
+		
+		while(goNext) {
+			System.out.println("Inserisci il nuovo desiderio: ");
+			String wish = sc.next();
+			wishList.add(wish);
+			
+			System.out.println("La lista è lunga " + wishList.size() );
+			System.out.println("Vuoi inserire un nuovo desiderio? ");
+			goNext = sc.nextBoolean();	
+		}
+		
+		sc.close();
+		
+		//ordino alfabeticamente la lista
+		wishList.sort(null);
+		
+		System.out.println(wishList);
+	}
+}
