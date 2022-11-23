6c8d7e352a4e4bfaf06a53137be29dfca5a51876
ErJump <fedelijunior@gmail.com>
2022-11-23 17:28:56 +0100

    refactor: :fire: Removes set import to Main class

--- src/org/generation/italy/christmas/Main.java
+++ src/org/generation/italy/christmas/Main.java
@@ -3,10 +3,8 @@ package org.generation.italy.christmas;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Scanner;
-import java.util.Set;
 
 public class Main {
 	public static void main(String[] args) {
